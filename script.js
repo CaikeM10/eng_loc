2659e870055b05557d6c213bb99e18c0a3b9f8bd
Caike  Marinho <caikemarinho150@gmail.com>
2025-06-19 10:12:21 -0300

    plataforma

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ document.addEventListener('DOMContentLoaded', function() {
             id: 13,
             name: "Plataforma",
             description: "Plataforma para elevação de materiais e pessoas em obras.",
-            image: "assets/images/escavadeira.jpg",
+            image: "/imgs/empilhadeira.png",
             specs: {    Mensal:"R$2.000/unit" ,  
                  Quantidades:"01 unidades disponiveis" ,
             
